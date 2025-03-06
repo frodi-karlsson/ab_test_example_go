34c9123fe721ff79b4e09fd046e23aab72e0ee64
Fróði Karlsson <frodikarlsson@gmail.com>
2025-03-06 23:44:01 +0100

    style: go fmt

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/frodi-karlsson/ab_test_example
+
+go 1.22.1
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"text/template"
-  "net"
 )
 
 const BUCKET_A_NAME = "bucket-a"
@@ -26,35 +26,35 @@ const TEMPLATE = `
 
 // Pretend this is a database or something
 var assignments map[string]int = map[string]int{
-  BUCKET_A_NAME: 0,
-  BUCKET_B_NAME: 0,
+	BUCKET_A_NAME: 0,
+	BUCKET_B_NAME: 0,
 }
 
 var tmpl = template.Must(template.New("index").Parse(TEMPLATE))
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  ip, _, err := net.SplitHostPort(r.RemoteAddr)
-  if err != nil {
-    fmt.Fprintf(w, "userip: %q is not IP:port\n", r.RemoteAddr)
-    return
-  }
-  lastChar := ip[len(ip)-1]
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		fmt.Fprintf(w, "userip: %q is not IP:port\n", r.RemoteAddr)
+		return
+	}
+	lastChar := ip[len(ip)-1]
 
-  // This sort of modulo thing is just an example of how users could be assigned.
-  // One benefit is that users in the same household will likely have a shared experience
-  // in case it comes up in conversation.
+	// This sort of modulo thing is just an example of how users could be assigned.
+	// One benefit is that users in the same household will likely have a shared experience
+	// in case it comes up in conversation.
 
-  if lastChar%2 == 0 {
-    assignments[BUCKET_A_NAME]++
-    tmpl.Execute(w, map[string]string{"bucket": BUCKET_A_NAME})
-  } else {
-    assignments[BUCKET_B_NAME]++
-    tmpl.Execute(w, map[string]string{"bucket": BUCKET_B_NAME})
-  }
+	if lastChar%2 == 0 {
+		assignments[BUCKET_A_NAME]++
+		tmpl.Execute(w, map[string]string{"bucket": BUCKET_A_NAME})
+	} else {
+		assignments[BUCKET_B_NAME]++
+		tmpl.Execute(w, map[string]string{"bucket": BUCKET_B_NAME})
+	}
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  fmt.Println("Listening on port 8080")
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	fmt.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
